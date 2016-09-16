e53722feeaf036852e0b724283e0050abc2dff9d
w205 <w205@ip-172-31-61-73.ec2.internal>
2016-09-16 04:37:43 +0000

    added hdfs destination

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -14,3 +14,11 @@ tail -n +2 'Readmissions and Deaths - Hospital.csv' > readmissions.csv
 tail -n +2 'Measure Dates.csv' > Measures.csv
 tail -n +2 'hvbp_hcahps_06_08_2016.csv' > surveys_responses.csv
 
+# Drop the renamed (and header trimmed) files into hdfs
+
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare
+hdfs dfs -put Measures.csv /user/w205/hospital_compare
+hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
+
