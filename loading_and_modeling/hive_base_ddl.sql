c9c1870f93f2f3b412f5d1420c9139d9f182f521
John Blakkan <john1@blakkan.net>
2016-09-26 04:08:09 +0000

    updated ddl statements

--- loading_and_modeling/hive_base_ddl.sql
+++ loading_and_modeling/hive_base_ddl.sql
@@ -1,33 +1,36 @@
+--
+-- hospitals table
+--
 DROP TABLE hospitals;
 CREATE EXTERNAL TABLE hospitals (
-	Provider_ID INT,
-	Hospital_Name STRING,
-	Address STRING,
-	City STRING,
-	State STRING,
-	ZIP_Code STRING,
-	County_Name STRING,
-	Phone_Number STRING,
-	Hospital_Type STRING,
-	Hospital_Ownership STRING,
-	Emergency_services STRING,
-	Meets_criteria_for_meaningful_use_of_EHRs STRING,
-	Hospital_overall_rating INT,
-	Hospital_overall_rating_footnote STRING,
-	Mortality_national_comparison STRING,
-	Mortality_national_comparison_footnote STRING,
-	Safety_of_care_national_comparison STRING,
-	Safety_of_care_national_comparision_footnote STRING,
-	Readmission_national_comparision STRING,
-	Readmission_national_comparision_footnote STRING,
-	Patient_experience_national_comparision STRING,
-	Patient_experience_national_comparision_footnote STRING,
-	Effectiveness_of_care_national_comparision STRING,
-	Effectiveness_of_care_national_comparision_footnote STRING,
-	Timeliness_of_care_national_comparision STRING,
-	Timeliness_of_care_national_comparision_footnote STRING,
-	Efficient_use_of_medical_imaging_national_comparision STRING,
-	Efficient_use_of_medical_imaging_national_comparison_footnote STRING )
+	provider_id INT,
+	hospital_name STRING,
+	address STRING,
+	city STRING,
+	state STRING,
+	zip_code STRING,
+	county_name STRING,
+	phone_number STRING,
+	hospital_type STRING,
+	hospital_ownership STRING,
+	emergency_services STRING,
+	meets_criteria_for_meaningful_use_of_ehrs STRING,
+	hospital_overall_rating STRING,   --There could be some NAs
+	hospital_overall_rating_footnote STRING,
+	mortality_national_comparison STRING,
+	mortality_national_comparison_footnote STRING,
+	safety_of_care_national_comparison STRING,
+	safety_of_care_national_comparision_footnote STRING,
+	readmission_national_comparision STRING,
+	readmission_national_comparision_footnote STRING,
+	patient_experience_national_comparision STRING,
+	patient_experience_national_comparision_footnote STRING,
+	effectiveness_of_care_national_comparision STRING,
+	effectiveness_of_care_national_comparision_footnote STRING,
+	timeliness_of_care_national_comparision STRING,
+	timeliness_of_care_national_comparision_footnote STRING,
+	efficient_use_of_medical_imaging_national_comparision STRING,
+	efficient_use_of_medical_imaging_national_comparison_footnote STRING )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 	"separatorChar" = ",",
@@ -36,24 +39,27 @@ WITH SERDEPROPERTIES(
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/hospitals';
 
+--
+-- effective_care table
+--
 DROP TABLE effective_care;
 CREATE EXTERNAL TABLE effective_care (
-	Provider_ID INT,
-	Hospital_Name STRING,
-	Address STRING,
-	City STRING,
-	State STRING,
-	ZIP_Code STRING,
-	County_Name STRING,
-	Phone_Number STRING,
-	Condition STRING,
-	Measure_ID STRING,
-	Measure_Name STRING,
-	Score STRING,
-	Sample STRING,
-	Footnote STRING,
-	Measure_Start_Date DATE,
-	Measure_End_Date DATE )
+	provider_id INT,
+	hospital_name STRING,
+	address STRING,
+	city STRING,
+	state STRING,
+	zip_code STRING,
+	county_name STRING,
+	phone_number STRING,
+	condition STRING,
+	measure_id STRING,
+	measure_name STRING,
+	score STRING,
+	sample STRING,
+	footnote STRING,
+	measure_start_date DATE,
+	measure_end_date DATE )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 	"separatorChar" = ",",
@@ -62,4 +68,101 @@ WITH SERDEPROPERTIES(
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/effective_care';
 
+--
+-- readmissions table
+--
+DROP TABLE readmissions;
+CREATE EXTERNAL TABLE readmissions (
+	provider_id INT,
+	hospital_name STRING,
+	address STRING,
+	city STRING,
+	state STRING,
+	zip_code STRING,
+	county_name STRING,
+	phone_number STRING,
+	measure_name STRING,
+	measure_id STRING,
+	compared_to_national STRING,
+	denominator INT,
+	score FLOAT,
+	lower_estimate FLOAT,
+	higher_estimate FLOAT,
+	footnote STRING,
+	measure_start_date DATE,
+	measure_end_date DATE )
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES(
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\' )
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/readmissions';
+
+--
+-- Measures table
+--
+DROP TABLE Measures;
+CREATE EXTERNAL TABLE measures (
+	measure_name STRING,
+	measure_id STRING,
+	measure_start_quarter STRING,
+	measure_start_date DATE,
+	measure_end_quarter STRING,
+	measure_end_date DATE )
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES(
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\' )
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/Measures';
+
+--
+-- surveys_responses table
+--   (note: provider id is provider_number in this table)
+--   (note: keeping overall ratings as strings, not ints)
+--
+DROP TABLE surveys_responses;
+CREATE EXTERNAL TABLE surveys_responses (
+	provider_number INT,
+	hospital_name STRING,
+	address STRING,
+	city STRING,
+	state STRING,
+	zip_code STRING,
+	county_name STRING,
+	communication_with_nurses_achievement_points STRING,
+	communication_with_nurses_improvement_points STRING,
+	communication_with_nurses_dimension_score STRING,
+	communication_with_doctors_achievement_points STRING,
+	communication_with_doctors_improvement_points STRING,
+	communication_with_doctors_dimension_score STRING,
+	responsiveness_of_hospital_staff_achievement_points STRING,
+	responsiveness_of_hospital_staff_improvement_points STRING,
+	responsiveness_of_hospital_staff_dimension_score STRING,
+	pain_management_achievement_points STRING,
+	pain_management_improvement_points STRING,
+	pain_management_dimension_score STRING,
+	communication_about_medicines_achievement_points STRING,
+	communication_about_medicines_improvement_points STRING,
+	communication_about_medicines_dimension_score STRING,
+	cleanliness_and_quietness_of_hospital_environment_achievement_points STRING,
+	cleanliness_and_quietness_of_hospital_environment_improvement_points STRING,
+	cleanliness_and_quietness_of_hospital_environment_dimension_score STRING,
+	discharge_information_achievement_points STRING,
+	discharge_information_improvement_points STRING,
+	discharge_information_dimension_score STRING,
+	overall_rating_of_hospital_achievement_points STRING,
+	overall_rating_of_hospital_improvement_points STRING,
+	overall_rating_of_hospital_dimension_score STRING,
+	hcahps_base_score STRING,    --keeping as string, since there may be not-availables
+	hcahps_consistency STRING )  --keeping as string, since there may be not-availables
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES(
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\' )
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/surveys_responses';
 
