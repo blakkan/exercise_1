caedbb33f09b48410317881bd1177e1b553a392a
w205 <w205@ip-172-31-5-213.ec2.internal>
2016-10-08 23:13:36 +0000

    Updated load_data_lake.sh to do wget instead of using static zip file

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -7,9 +7,14 @@
 # get the url and unzip it (just unzipping the files needed)
 #
 
-rm *.csv
+rm *.csv *.zip
 
-### don't do the wget anymore, just leave in repo wget http://medicare.gov/download/HospitalCompare/2015/July/HOSArchive_Revised_FlatFiles_20150716.zip
+echo "*****completed removing old files"
+
+wget -O Hospital_Revised_Flatfiles.zip "https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip"
+
+echo "*****completed the wget"
+ls -l Hospital_Revised_Flatfiles.zip
 
 unzip Hospital_Revised_Flatfiles.zip \
  'Hospital General Information.csv' \
@@ -18,6 +23,7 @@ unzip Hospital_Revised_Flatfiles.zip \
  'Measure Dates.csv' \
  'hvbp_hcahps_05_28_2015.csv'
 
+echo "*****completed the unzip"
 #
 # rename and remove header lines
 #
@@ -30,8 +36,12 @@ tail -n +2 'Readmissions and Deaths - Hospital.csv' > readmissions.csv
 tail -n +2 'Measure Dates.csv' > Measures.csv
 tail -n +2 'hvbp_hcahps_05_28_2015.csv' > surveys_responses.csv
 
+echo "*****completed the removal of the header line and copying into shorter-named csvs"
+ls *.csv
+
 # Drop the renamed (and header trimmed) files into hdfs
 
+hdfs dfs -rm -r /user/w205/hospital_compare  # out with the old
 
 hdfs dfs -mkdir /user/w205/hospital_compare
 
@@ -50,9 +60,13 @@ hdfs dfs -put Measures.csv /user/w205/hospital_compare/Measures
 hdfs dfs -mkdir /user/w205/hospital_compare/surveys_responses
 hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare/surveys_responses
 
+echo "*****completed purging old hdfs files an adding new ones"
+
 # cleanup csv and files here; just want the files in distributed file sysstem
 
-rm *.csv 
+rm *.csv *.zip
+
+echo "*****completed removal of local non hdfs csv and zip files"
 
 # at this point, we have the five csv files in the distributed file system.
 # we haven't yet put any database ddl or access capability around them, just
